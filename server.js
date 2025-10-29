89e84d0f71e58f5d44d578da2558a9aff82b6c94
bytebandit,viron <mindedbusines@gmail.com>
2025-10-29 19:03:44 +0000

    delete route added , patch re configured

--- server.js
+++ server.js
@@ -4,52 +4,66 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const fs = require('fs');
-const { notStrictEqual } = require('assert');
 const PORT = process.env.PORT;
 
-
 //middleware
- app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(express.json());
 const accessLog = fs.createWriteStream(path.join(__dirname,'access.log'),{flag:'a'});
 app.use(morgan('combined',{stream:accessLog}));
-
-
 
 //the note array
 let note =[
 	{"id":1,
-	"title":"my story",
-	"content":"i was 10 years old when i was 2"
+	"title":"Group4 -assignment",
+	"content":"create a note taking api ",
+	"date": new Date().toDateString()
+	 
 }
 ];
 
 //isServer up
 app.get('/',(req,res)=>{
-	res.send('server active');
+	res.send('server active 😁');
 });
+
 //get every note
 app.get('/note',(req,res)=>{
 	res.status(200).json(note)
 });
+
 //post route create new note
 app.post('/note',(req,res)=>{
-const newNote = {id:note.length +1, ...req.body};
+const newNote = {id:note.length +1, ...req.body,	"date": new Date().toDateString()};
 if(!req.body.title || !req.body.content){
 	res.status(404).json({error:"title and body is neede"})
 }
 note.push(newNote);
-res.status(201).json(newNote)
+res.status(201).json(note)
+
 });
 
 //patch route replace note
 
-app.patch('/note/edit/:id', (req, res) => {
-   const notez = note.find((t) => t.id === parseInt(req.params.id)); 
+app.patch('/note/:id', (req, res) => {
+ const notez = note.find((t) => t.id === parseInt(req.params.id));
+
   if (!notez) return res.status(404).json({ message: 'note does not exit' });
   Object.assign(notez, req.body);
   res.status(200).json(notez);
 
+});
 
+app.delete('/note/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const initialLength = note.length;
+  const notez = note.filter((t) => t.id !== id); 
+  if (notez.length === initialLength){
+    return res.status(404).json({ error: 'Not found' });
+	}
+	note = notez
+
+	res.status(204).send();
 });
 
 
